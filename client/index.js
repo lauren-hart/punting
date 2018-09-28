cd50b246c1969766de28490afceed7b5641e8d34
Lauren Hart <laurenannehart@gmail.com>
2018-09-28 21:03:15 +1200

    created the store in client/index.js

--- client/index.js
+++ client/index.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import {Provider} from 'react-redux'
-// import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import {HashRouter as Router} from 'react-router-dom'
 
-// import reducers from './reducers'
+import reducers from './reducers'
 import App from './components/App'
 
-// const store = createStore(reducers,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// )
+const store = createStore(reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    // <Provider store={store}>
-    <Router>
-      <div>
-        <App />
-      </div>
-    </Router>,
+    <Provider store={store}>
+      <Router>
+        <div>
+          <App />
+        </div>
+      </Router>,
     // </Provider>,
     document.getElementById('app'))
 })
